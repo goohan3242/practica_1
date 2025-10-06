f5ba57a8ce1d4eae99725b9076cc75717de945ae
goohan3242 <goohan3242@gmail.com>
2025-10-06 22:13:37 +0000

    feat[Punto_A.sh]: Agrego la respuesta

---
+++ RTA_Examen_20251006/Punto_A.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+
+
+echo "Creo la estructura: "
+mkdir -p $HOME/Astronomia/{Cometa/{Cola,Gravedad,Orbita},Planeta/{Gravedad,Orbita}}
+
+
+echo "Muestro la misma:"
+tree $HOME/Astronomia/
+
+
